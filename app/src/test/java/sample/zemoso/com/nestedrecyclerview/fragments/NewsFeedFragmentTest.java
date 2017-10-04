ba718f2b62e2c3c35157bff7c9941bf018cd3da9
MohammedAtif <atif@zemosolabs.com>
2017-10-04 17:00:19 +0530

    Test cases

---
+++ app/src/test/java/sample/zemoso/com/nestedrecyclerview/fragments/NewsFeedFragmentTest.java
@@ -0,0 +1,33 @@
+package sample.zemoso.com.nestedrecyclerview.fragments;
+
+import android.support.v7.widget.RecyclerView;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author vin
+ * Created on 4/10/17.
+ */
+public class NewsFeedFragmentTest {
+
+    private NewsFeedFragment newsFeedFragment;
+    private RecyclerView recyclerView, recyclerView2;
+
+    @Before
+    public void newInstance() throws Exception {
+//        newsFeedFragment = NewsFeedFragment.newInstance();
+//        recyclerView = new RecyclerView(newsFeedFragment.getContext());
+//        recyclerView2 = new RecyclerView(newsFeedFragment.getContext());
+    }
+
+    @Test
+    public void updateStoryList() throws Exception {
+//        assertTrue("Should return true for new object", newsFeedFragment.updateStoryList(recyclerView));
+//        assertFalse("Should return false for same object", newsFeedFragment.updateStoryList(recyclerView));
+//        assertTrue("Should return true for new object", newsFeedFragment.updateStoryList(recyclerView2));
+    }
+
+}
